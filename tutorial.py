28c8cb2c3388293ec4ab6d3feb9ea4061d81f3ea
Michal.Tkaczyk <michaltkaczyk@outlook.com>
2023-08-03 16:13:33 +0200

    add start_screen

--- tutorial.py
+++ tutorial.py
@@ -1,14 +1,18 @@
 import curses
 from curses import wrapper
 
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to the Speed Typing Test!")
+    stdscr.addstr("\nPress any key to begin.")
+    stdscr.refresh()
+    stdscr.getkey()
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
-
-    stdscr.clear()
-    stdscr.addstr(1, 5, "Hello world!", curses.color_pair(1))
-    stdscr.refresh()
-    stdscr.getkey()
+    
+    start_screen(stdscr)
 
 wrapper(main)
