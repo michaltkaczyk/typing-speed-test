84b5a2248068ef80bdf61ec0978f249609c7ee6a
Michal.Tkaczyk <michaltkaczyk@outlook.com>
2023-08-03 16:16:02 +0200

    add wpm_test

--- tutorial.py
+++ tutorial.py
@@ -8,11 +8,21 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+def wpm_test(stdscr):
+    target_text = "Some test text for this app!"
+    current_text = []
+
+    stdscr.clear()
+    stdscr.addstr(target_text)
+    stdscr.refresh()
+    stdscr.getkey()
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     
     start_screen(stdscr)
+    wpm_test(stdscr)
 
 wrapper(main)
